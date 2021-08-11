0d2e54157cb37263ab17f428fd8c2c8c4a6af21d
Jeannetta Marian <jmarian@et-j1.kzn.21-school.ru>
2021-08-11 10:04:37 +0300

    изменение в строках

--- Modul 00/ex01/class_phonebook.cpp
+++ Modul 00/ex01/class_phonebook.cpp
@@ -6,27 +6,24 @@
 /*   By: jmarian <jmarian@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 14:07:50 by jmarian           #+#    #+#             */
-/*   Updated: 2021/08/11 09:39:24 by jmarian          ###   ########.fr       */
+/*   Updated: 2021/08/11 10:03:29 by jmarian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.h"
 
-void	Phone:: add_phone(char *contacts)
+void	Phone:: add_phone(int num_cont)
 {
-	int i;
-	i = 0;
-
 	std::cout << "first name:" << "\n";
-	std::cin >> contacts[i++];
+	getline(std::cin, first_name[num_cont]);
 	std::cout << "last name:" << "\n";
-	std::cin >> contacts[i++];
+	getline(std::cin, last_name[num_cont]);
 	std::cout << "nickname:" << "\n";
-	std::cin >> contacts[i++];
+	getline(std::cin, nickname[num_cont]);
 	std::cout << "phone number:" << "\n";
-	std::cin >> contacts[i++];
+	getline(std::cin, phone_number[num_cont]);
 	std::cout << "darkest secret:" << "\n";
-	std::cin >> contacts[i++];
+	getline(std::cin, darkest_secret[num_cont]);
 }
 
 void	Phone:: serch_phone(void)
--- Modul 00/ex01/phonebook.h
+++ Modul 00/ex01/phonebook.h
@@ -6,15 +6,15 @@
 class Phone
 {
 	public://все что после нее публичная 
-	char contacts[8][5];
-	/*
-	std::string first_name;
-	std::string last_name;
-	std::string  nickname;
-	std::string phone_number;
-	std::string darkest_secret;
-	*/
-	void	add_phone(char *contacts);
+	//char contacts[8][5];
+	int			num_cont;
+	std::string	first_name[8];
+	std::string	last_name[8];
+	std::string	nickname[8];
+	std::string	phone_number[8];
+	std::string	darkest_secret[8];
+	
+	void	add_phone(int num_cont);
 	void	serch_phone(void);
 };
 
