3808e5518b10431328a8f7e7e7b07a7684cffc34
Jeannetta Marian <jmarian@er-a1.kzn.21-school.ru>
2021-08-18 10:51:08 +0300

    сделано ех00 мд01

---
+++ Modul_02/ex00/main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Jeannetta Marian on 8/17/21.
+//
+
+#include "Fixed.hpp"
+
+#include <iostream>
+int main( void ) {
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return (0);
+}
---
+++ Modul_02/ex01/Fixed.hpp
@@ -0,0 +1,23 @@
+//
+// Created by Jeannetta Marian on 8/17/21.
+//
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed{
+public:
+    Fixed();
+    Fixed(Fixed const &same_class);
+    ~Fixed();
+    int getRawBits( void ) const;
+    void setRawBits( int const raw );
+    Fixed & operator = (const Fixed &same_class);
+private:
+    int number;
+    static const int bite = 8;
+};
+
+#endif
